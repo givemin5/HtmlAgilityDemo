fd2bfc247b9da0dc54c5f2b285f48c8b60c5881a
givemin5 <givemin5@gmail.com>
2018-09-02 12:10:15 +0800

    Add project files.

---
+++ Givemin5.HtmlAgilityDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Givemin5.HtmlAgilityDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = $"https://www.books.com.tw/products/0010553999";
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            var nameNode = doc.DocumentNode.SelectSingleNode("//h1[@itemprop='name']");
+
+            Console.WriteLine(nameNode.InnerText);
+            Console.ReadKey();
+        }
+    }
+}
